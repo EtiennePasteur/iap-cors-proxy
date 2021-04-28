630809907b3f0eda15f8801737fd3a79d4f5c655
Etienne PASTEUR <me@etiennepasteur.com>
2021-04-28 14:22:39 +0200

    fix: Add support for localhost backend

--- src/iap-proxy.js
+++ src/iap-proxy.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const https = require('https');
 const url = require('url');
 const {createProxyMiddleware} = require('http-proxy-middleware');
 const {GoogleAuth} = require('google-auth-library');
@@ -28,10 +27,16 @@ module.exports = {
     },
 
     iapAuth: async (addr, keyFile = process.env.GOOGLE_APPLICATION_CREDENTIALS, renew = false) => {
-        https.request(addr, {method: 'HEAD'}, res => {
-            const queryObject = url.parse(res.headers.location, true).query;
-            const client_id = queryObject.client_id;
-            module.exports.getToken(addr, keyFile, client_id, renew);
+        const adapters = {
+            'http:': require('http'),
+            'https:': require('https')
+        };
+        adapters[url.parse(addr).protocol].request(addr, {method: 'HEAD'}, res => {
+            if (res.headers.location) {
+                const queryObject = url.parse(res.headers.location, true).query;
+                const client_id = queryObject.client_id;
+                module.exports.getToken(addr, keyFile, client_id, renew);
+            }
         }).end();
     },
 
@@ -41,17 +46,26 @@ module.exports = {
 
     onProxyReq: (proxyReq, req, res) => {
         console.log('🛰️ ' + req.url);
+
+        res.header("Access-Control-Allow-Origin", module.exports.frontendUrl);
+        res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+        res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+        if (req.method === 'OPTIONS') {
+            return res.status(200).end();
+        }
+
         proxyReq.setHeader('Cookie', 'GCP_IAAP_AUTH_TOKEN=' + module.exports.cookie);
     },
 
-    launchExpress: (back, front, keyfile, port, prefix) => {
+    launchExpress: (back, front, keyfile, port, pathRewrite) => {
         if (module.exports.server === null) {
             const app = express();
-            app.use(prefix, createProxyMiddleware({
+            app.use(createProxyMiddleware({
                 target: back,
                 changeOrigin: true,
                 onProxyReq: module.exports.onProxyReq,
-                onProxyRes: module.exports.onProxyRes
+                onProxyRes: module.exports.onProxyRes,
+                pathRewrite: {[pathRewrite]: ''}
             }));
             module.exports.server = require('http').createServer(app);
             module.exports.server = require('http-shutdown')(module.exports.server);
@@ -67,7 +81,7 @@ module.exports = {
         }
     },
 
-    start: (back, front, keyfile, port, prefix) => {
+    start: (back, front, keyfile, port, pathRewrite) => {
         if (!module.exports.intervalRef) {
             module.exports.intervalRef = setInterval(() => {
                 module.exports.iapAuth(back, keyfile, true)
@@ -75,10 +89,10 @@ module.exports = {
         }
 
         if (module.exports.cookie !== null) {
-            module.exports.launchExpress(back, front, keyfile, port, prefix);
+            module.exports.launchExpress(back, front, keyfile, port, pathRewrite);
         } else {
             module.exports.iapAuth(back, keyfile).then(() => {
-                module.exports.launchExpress(back, front, keyfile, port, prefix);
+                module.exports.launchExpress(back, front, keyfile, port, pathRewrite);
             });
         }
     },
